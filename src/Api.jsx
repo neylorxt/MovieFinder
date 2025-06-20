7ffe3cfc954da4e03b0aa92c376f340ce7dac0de
NEYLORXT <neylorxt@gmail.com>
2025-06-20 15:25:17 +0200

    V0.2.2
    Update
    - Api

--- src/Api.jsx
+++ src/Api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const BASE_URL_FILMS = "/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
-const API_BEAR = "Your API key";
+const API_BEAR = import.meta.env.VITE_TMDB_API_KEY;
 
 export const getMovies = async () => {
     try {
@@ -44,7 +44,7 @@ export const getMovieByName = async (movieName) => {
                 },
                 headers: {
                     accept: 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZWI5NjE3N2E3YTk0ZTY2ZmYwZWNjN2EwNWE1YTkwZSIsIm5iZiI6MTc1MDI3MTAxNS42NzEsInN1YiI6IjY4NTMwNDI3ZDExMjA0ZTA1ZDYwYjM0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xZTlhJb-XRzCAdDJyoOztXNHUlvVWxB8uw_6OZKIPwQ'
+                    Authorization: `Bearer ${API_BEAR}`
                 }
             }
         );
